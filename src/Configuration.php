3aabb46f0d9597ba7f99936951313e2d9ef88901
Guido Falsi <mad@madpilot.net>
2024-05-13 22:59:41 +0200

    Create Configuration class.

---
+++ src/Configuration.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace madpilot78\FreeBoxPHP;
+
+use madpilot78\FreeBoxPHP\Enum\BoxType;
+
+final readonly class Configuration
+{
+    public const string VERSION = '0.0.1';
+
+    public const string DEFAULT_APPID = 'net.madpilot.freeboxphp';
+    public const string DEFAULT_APPNAME = 'FreeBoxPHP';
+    public const string DEFAULT_HOSTNAME = 'mafreebox.freebox.fr';
+    public const string DEFAULT_DEVICENAME = 'Unknwon';
+    public const bool DEFAULT_HTTPS = true;
+    public const bool DEFAULT_LOCAL_ACCESS = true;
+    public const string DEFAULT_BOX_TYPE = 'FreeBox';
+    public const int DEFAULT_TIMEOUT = 30;
+    public const string CERT_PATH = '/data';
+
+    public ?string $certFile;
+    public string $deviceName;
+
+    public function __construct(
+        public string $appId = self::DEFAULT_APPID,
+        public string $appName = self::DEFAULT_APPNAME,
+        public string $hostname = self::DEFAULT_HOSTNAME,
+        public bool $https = self::DEFAULT_HTTPS,
+        public bool $localAccess = self::DEFAULT_LOCAL_ACCESS,
+        public BoxType $boxType = BoxType::Free,
+        public int $timeout = self::DEFAULT_TIMEOUT,
+        string $deviceName = self::DEFAULT_DEVICENAME,
+        ?string $certFile = '',
+    ) {
+        $this->certFile = $certFile  === ''
+            ? realpath(__DIR__ . '/../' . self::CERT_PATH . '/' . $this->boxType->value . '.pem')
+            : $certFile;
+
+        $machineHostname = gethostname();
+        $this->deviceName = $deviceName === self::DEFAULT_DEVICENAME && strlen($machineHostname)
+            ? $machineHostname
+            : $deviceName;
+    }
+
+    public function isDefaulthostname(): bool
+    {
+        return $this->hostname === self::DEFAULT_HOSTNAME;
+    }
+}
---
+++ tests/Unit/ConfigurationTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use madpilot78\FreeBoxPHP\Configuration;
+
+class ConfigurationTest extends TestCase
+{
+    public function testDefaultConfiguration(): void
+    {
+        $configuration = new Configuration();
+
+        $this->assertEquals('net.madpilot.freeboxphp', $configuration->appId);
+        $this->assertEquals('FreeBoxPHP', $configuration->appName);
+        $this->assertEquals(
+            realpath(__DIR__ . '/../../data/FreeBox.pem'),
+            $configuration->certFile,
+        );
+        $this->assertEquals(gethostname(), $configuration->deviceName);
+
+        $this->assertTrue($configuration->isDefaulthostname());
+    }
+
+    public function testCustomConfiguration(): void
+    {
+        $configuration = new Configuration(
+            hostname: 'box.example.org',
+            certFile: null,
+            deviceName: 'DeepThought',
+        );
+
+        $this->assertNull($configuration->certFile);
+        $this->assertEquals('DeepThought', $configuration->deviceName);
+        $this->assertEquals('box.example.org', $configuration->hostname);
+        $this->assertFalse($configuration->isDefaulthostname());
+    }
+}
