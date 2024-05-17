f08fe657c1d734c15b3afc1369c0b5dcbd57ca4c
Guido Falsi <mad@madpilot.net>
2024-05-17 23:25:48 +0200

    Add Guzzle wrapper class.

---
+++ src/HttpClient.php
@@ -0,0 +1,109 @@
+<?php
+
+declare(strict_types=1);
+
+namespace madpilot78\FreeBoxPHP;
+
+use GuzzleHttp\Exception\ClientException;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\ResponseInterface;
+use madpilot78\FreeBoxPHP\Exception\ApiAuthException;
+use madpilot78\FreeBoxPHP\Exception\ApiErrorException;
+use madpilot78\FreeBoxPHP\Exception\NetworkErrorException;
+
+class HttpClient
+{
+    public function __construct(private ClientInterface $client) {}
+
+    /**
+     * @throws NetworkErrorException
+     */
+    private function checkStatusCode(ResponseInterface $response): void
+    {
+        $code = $response->getStatusCode();
+
+        if ($code != 200) {
+            throw new NetworkErrorException(
+                'Unexpected HTTP status code (' . $code . '): ' . $response->getReasonPhrase(),
+                $code,
+            );
+        }
+    }
+
+    /**
+     * @throws ApiErrorException
+     */
+    private function bodyToJson(ResponseInterface $response, array $reqResult = [], bool $checkStatus = true): array
+    {
+        if ($checkStatus) {
+            $this->checkStatusCode($response);
+        }
+
+        $rawBody = (string) $response->getBody();
+        if (!json_validate($rawBody)) {
+            throw new ApiErrorException('Invalid JSON in body');
+        }
+
+        $json = json_decode($rawBody, true);
+
+        if (!empty($reqResult)) {
+            if (!array_key_exists('success', $json) || !$json['success']) {
+                throw new ApiErrorException('', $json);
+            }
+
+            if (!array_key_exists('result', $json)) {
+                throw new ApiErrorException(ApiErrorException::RESULT_MISSING);
+            }
+
+            foreach ($reqResult as $req) {
+                if (!array_key_exists($req, $json['result'])) {
+                    throw new ApiErrorException(ApiErrorException::FIELD_MISSING . ': ' . $req);
+                }
+            }
+
+            // Only return the result
+            $json = $json['result'];
+        }
+
+        return $json;
+    }
+
+    /**
+     * Wrap Guzzle client.
+     *
+     * If first argument is an Array grab it for own use. Otherwise it
+     * is expected to be a method or an URL.
+     *
+     * @throws ApiAuthException
+     * @throws ClientException
+     */
+    public function __call(string $name, array $arguments): array
+    {
+        $reqResults = [];
+        if (is_array($arguments[0])) {
+            $reqResults = array_shift($arguments);
+        }
+
+        try {
+            $response = $this->client->$name(...$arguments);
+        } catch (ClientException $e) {
+            $response = $e->getResponse();
+            $statusCode = $response->getStatusCode();
+
+            switch ($statusCode) {
+                case 403:
+                    $error = $this->bodyToJson($response, checkStatus: false);
+                    throw new ApiAuthException($error['msg'] ?? 'Unknown error', $statusCode, $e);
+                    break;
+
+                default:
+                    throw $e;
+                    break;
+            }
+        }
+
+        $this->checkStatusCode($response);
+
+        return $this->bodyToJson($response, $reqResults, false);
+    }
+}
---
+++ tests/Unit/HttpClientTest.php
@@ -0,0 +1,194 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ServerException;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\Psr7\Response;
+use PHPUnit\Framework\TestCase;
+use madpilot78\FreeBoxPHP\Exception\ApiAuthException;
+use madpilot78\FreeBoxPHP\Exception\ApiErrorException;
+use madpilot78\FreeBoxPHP\Exception\NetworkErrorException;
+use madpilot78\FreeBoxPHP\HttpClient;
+
+class HttpClientTest extends TestCase
+{
+    private HttpClient $httpClient;
+    private MockHandler $mock;
+
+    private const URL = 'https://www.test.org/test';
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->mock = new MockHandler();
+
+        $handlerStack = HandlerStack::create($this->mock);
+
+        $this->httpClient = new HttpClient(new Client(['handler' => $handlerStack]));
+    }
+
+    public function testNotJson(): void
+    {
+        $this->mock->append(new Response(body: 'Test string, not json.'));
+
+        $this->expectException(ApiErrorException::class);
+        $this->httpClient->get(self::URL);
+    }
+
+    public function testNotAuthNoMsg(): void
+    {
+        $this->mock->append(
+            new Response(
+                status: 403,
+                headers: ['Content-Type' => 'application/json'],
+                body: json_encode([
+                    'status' => 'Access denied',
+                ]),
+            ),
+        );
+
+        $this->expectException(ApiAuthException::class);
+        $this->expectExceptionMessage('Unknown error');
+        $this->httpClient->get(self::URL);
+    }
+
+    public function testNotAuthWithMsg(): void
+    {
+        $this->mock->append(
+            new Response(
+                status: 403,
+                headers: ['Content-Type' => 'application/json'],
+                body: json_encode([
+                    'msg' => 'Access denied',
+                ]),
+            ),
+        );
+
+        $this->expectException(ApiAuthException::class);
+        $this->expectExceptionMessage('Access denied');
+        $this->httpClient->get(self::URL);
+    }
+
+    public function testNotFound(): void
+    {
+        $this->mock->append(new Response(status: 404, body: 'Not found'));
+
+        $this->expectException(ClientException::class);
+        $this->httpClient->get(self::URL);
+    }
+
+    public function testServerError(): void
+    {
+        $this->mock->append(new Response(status: 500, body: 'Server error'));
+
+        $this->expectException(ServerException::class);
+        $this->httpClient->get(self::URL);
+    }
+
+    public function testNot200Response(): void
+    {
+        $this->mock->append(new Response(status: 201, body: 'Created'));
+
+        $this->expectException(NetworkErrorException::class);
+        $this->httpClient->get(self::URL);
+    }
+
+    public function testJsonNoRequired(): void
+    {
+        $this->mock->append(
+            new Response(
+                status: 200,
+                headers: ['Content-Type' => 'application/json'],
+                body: json_encode([
+                    'foo' => 'bar',
+                ]),
+            ),
+        );
+
+        $content = $this->httpClient->get(self::URL);
+
+        $this->assertArrayHasKey('foo', $content);
+        $this->assertEquals('bar', $content['foo']);
+    }
+
+    public function testJsonRequiredMissing(): void
+    {
+        $this->mock->append(
+            new Response(
+                status: 200,
+                headers: ['Content-Type' => 'application/json'],
+                body: json_encode([
+                    'success' => true,
+                    'result' => [
+                        'foo' => 'bar',
+                    ],
+                ]),
+            ),
+        );
+
+        $this->expectException(ApiErrorException::class);
+        $this->httpClient->get(['baz'], self::URL);
+    }
+
+    public function testJsonRequiredNoResult(): void
+    {
+        $this->mock->append(
+            new Response(
+                status: 200,
+                headers: ['Content-Type' => 'application/json'],
+                body: json_encode([
+                    'success' => true,
+                ]),
+            ),
+        );
+
+        $this->expectException(ApiErrorException::class);
+        $this->httpClient->get(['foo'], self::URL);
+    }
+
+    public function testJsonRequiredNoSuccess(): void
+    {
+        $this->mock->append(
+            new Response(
+                status: 200,
+                headers: ['Content-Type' => 'application/json'],
+                body: json_encode([
+                    'result' => [
+                        'foo' => 'bar',
+                    ],
+                ]),
+            ),
+        );
+
+        $this->expectException(ApiErrorException::class);
+        $this->httpClient->get(['foo'], self::URL);
+    }
+
+    public function testJsonRequiredGood(): void
+    {
+        $this->mock->append(
+            new Response(
+                status: 200,
+                headers: ['Content-Type' => 'application/json'],
+                body: json_encode([
+                    'success' => true,
+                    'result' => [
+                        'foo' => 'bar',
+                    ],
+                ]),
+            ),
+        );
+
+        $content = $this->httpClient->get(['foo'], self::URL);
+
+        $this->assertArrayHasKey('foo', $content);
+        $this->assertEquals('bar', $content['foo']);
+    }
+}
