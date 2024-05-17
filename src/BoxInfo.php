e3af53c83ad14ed57165f037d27cc933d9ef9748
Guido Falsi <mad@madpilot.net>
2024-05-18 00:09:34 +0200

    Add BoxInfo class to contain runtime data about the box.

---
+++ src/BoxInfo.php
@@ -0,0 +1,107 @@
+<?php
+
+declare(strict_types=1);
+
+namespace madpilot78\FreeBoxPHP;
+
+use InvalidArgumentException;
+use OutOfBoundsException;
+
+class BoxInfo
+{
+    private const array REQUIRED = [
+        'api_base_url',
+        'device_name',
+        'https_available',
+        'api_domain',
+        'api_version',
+    ];
+
+    private array $info = [];
+    private ?string $apiUrl = null;
+
+    public function __construct(private Configuration $config) {}
+
+    /**
+     * the api_version call should return a flat json
+     * object with some properties.
+     *
+     * NOTE: Some can be overridden by configuration.
+     *
+     * @throws InvalidArgumentException
+     */
+    public function save(array $data): void
+    {
+        $req = self::REQUIRED;
+
+        foreach ($data as $name => $value) {
+            if (is_array($value) || is_object($value)) {
+                throw new InvalidArgumentException('Invalid json returned');
+            }
+
+            if (in_array($name, $req)) {
+                unset($req[array_search($name, $req)]);
+            }
+
+            $this->info[$name] = $value;
+        }
+
+        if (!empty($req)) {
+            throw new InvalidArgumentException(
+                'Returned json missing required propertie(s): ' . implode(', ', $req),
+            );
+        }
+
+        if (!$this->config->isDefaulthostname()) {
+            $this->info['api_domain'] = $this->config->hostname;
+        }
+    }
+
+    private function makeApiUrl(): ?string
+    {
+        if (
+            !isset($this->info['api_domain']) ||
+            !isset($this->info['api_base_url']) ||
+            !isset($this->info['api_version'])
+        ) {
+            return null;
+        }
+
+        $major = substr($this->info['api_version'], 0, strpos($this->info['api_version'], '.'));
+
+        if ($this->info['https_available']) {
+            $port = $this->config->localAccess ? '' : ':' . $this->info['https_port'];
+            $scheme = 'https';
+        } else {
+            $port = '';
+            $scheme = 'http';
+        }
+
+        return $scheme . '://' . $this->info['api_domain'] . $port . $this->info['api_base_url'] . 'v' . $major;
+    }
+
+    /**
+     * @throws OutOfBoundsException
+     */
+    public function __get(string $name): null|bool|int|string
+    {
+        if ($name === 'apiUrl') {
+            if (is_null($this->apiUrl)) {
+                $this->apiUrl = $this->makeApiUrl();
+            }
+
+            return $this->apiUrl;
+        }
+
+        if (array_key_exists($name, $this->info)) {
+            return $this->info[$name];
+        }
+
+        throw new OutOfBoundsException('Property ' . $name . ' not found', 101);
+    }
+
+    public function __isset(string $name): bool
+    {
+        return isset($this->info[$name]);
+    }
+}
---
+++ tests/Unit/BoxInfoTest.php
@@ -0,0 +1,113 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use InvalidArgumentException;
+use OutOfBoundsException;
+use PHPUnit\Framework\TestCase;
+use StdClass;
+use madpilot78\FreeBoxPHP\BoxInfo;
+use madpilot78\FreeBoxPHP\Configuration;
+
+class BoxInfoTest extends TestCase
+{
+    private const array INFO = [
+        'api_base_url' => '/api/',
+        'device_name' => 'thisBox',
+        'https_available' => true,
+        'https_port' => 443,
+        'api_domain' => 'example.net',
+        'api_version' => '10.1',
+    ];
+
+    private BoxInfo $boxInfo;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->boxInfo = new BoxInfo(new Configuration());
+    }
+
+    public function testSavingBoxinfoArray(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid json returned');
+        $this->boxInfo->save([
+            'foo' => ['bar', 'baz'],
+        ]);
+    }
+
+    public function testSavingBoxinfoObject(): void
+    {
+        $obj = new StdClass();
+        $obj->bar = 'baz';
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid json returned');
+        $this->boxInfo->save([
+            'foo' => $obj,
+        ]);
+    }
+
+    public function testSavingBoxinfoMissing(): void
+    {
+        $info = self::INFO;
+
+        unset($info['api_version']);
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Returned json missing required propertie(s): api_version');
+        $this->boxInfo->save($info);
+    }
+
+    public function testBoxInfoGetValue(): void
+    {
+        $this->boxInfo->save(self::INFO);
+
+        $this->assertEquals(self::INFO['api_domain'], $this->boxInfo->api_domain);
+        $this->assertTrue(self::INFO['https_available']);
+    }
+
+    public function testBoxInfoGetValueNotSaved(): void
+    {
+        $this->boxInfo->save(self::INFO);
+
+        $this->expectException(OutOfBoundsException::class);
+        $result = $this->boxInfo->foo;
+    }
+
+    public function testBoxInfoGetApiUrl(): void
+    {
+        $this->boxInfo->save(self::INFO);
+        $major = substr(self::INFO['api_version'], 0, strpos(self::INFO['api_version'], '.'));
+
+        $this->assertEquals(
+            'https://' . self::INFO['api_domain'] . self::INFO['api_base_url'] . 'v' . $major,
+            $this->boxInfo->apiUrl,
+        );
+    }
+
+    public function testBoxInfoGetApiUrlNonLocal(): void
+    {
+        $this->boxInfo = new BoxInfo(new Configuration(localAccess: false));
+        $this->boxInfo->save(self::INFO);
+        $major = substr(self::INFO['api_version'], 0, strpos(self::INFO['api_version'], '.'));
+
+        $this->assertEquals(
+            'https://' . self::INFO['api_domain'] . ':' . self::INFO['https_port'] . self::INFO['api_base_url'] . 'v' . $major,
+            $this->boxInfo->apiUrl,
+        );
+    }
+
+    public function testBoxInfoNonDefaultHostname(): void
+    {
+        $custom = 'myhost.example.org';
+        $this->boxInfo = new BoxInfo(new Configuration(hostname: $custom));
+        $this->boxInfo->save(self::INFO);
+
+        $this->assertEquals($custom, $this->boxInfo->api_domain);
+    }
+}
