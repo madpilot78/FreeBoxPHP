e4cae849ce84c85cae00db2f0a2cd7f0431ce2f6
Guido Falsi <mad@madpilot.net>
2024-05-18 19:15:35 +0200

    Add an AuthManager to manage secrets.

---
+++ src/Auth/Manager.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace madpilot78\FreeBoxPHP\Auth;
+
+use madpilot78\FreeBoxPHP\Exception\MissingAuthException;
+
+class Manager
+{
+    private ?string $authToken = null;
+    private ?string $sessionToken = null;
+    private ?string $currentChallenge = null;
+    private array $permissions = [];
+
+    public function setAuthToken(string $token): self
+    {
+        $this->authToken = $token;
+
+        return $this;
+    }
+
+    public function setSessionToken(string $token): self
+    {
+        $this->sessionToken = $token;
+
+        return $this;
+    }
+
+    public function getSessionToken(): ?string
+    {
+        return $this->sessionToken;
+    }
+
+    public function setChallenge(string $challenge): self
+    {
+        $this->currentChallenge = $challenge;
+
+        return $this;
+    }
+
+    public function hasChallenge(): bool
+    {
+        return isset($this->currentChallenge);
+    }
+
+    public function setPermissions(array $permissions): self
+    {
+        $this->permissions = $permissions;
+
+        return $this;
+    }
+
+    public function hasPermission(string $permission): bool
+    {
+        return in_array($permission, $this->permissions);
+    }
+
+    /**
+     * @throws MissingAuthException
+     */
+    public function getPassword(?string $challenge = null): string
+    {
+        if (is_null($this->authToken)) {
+            throw new MissingAuthException('Authorization Token missing');
+        }
+
+        if (!$this->hasChallenge() && is_null($challenge)) {
+            throw new MissingAuthException('No Challenge provided');
+        }
+
+        return hash_hmac('sha1', $challenge ?? $this->currentChallenge, $this->authToken);
+    }
+}
---
+++ tests/Unit/Auth/ManagerTest.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use madpilot78\FreeBoxPHP\Auth\Manager as AuthManager;
+use madpilot78\FreeBoxPHP\Exception\MissingAuthException;
+
+class ManagerTest extends TestCase
+{
+    private const HASH = '0c534563a85c1e77106850f4fe745daa1be1ebea';
+    private AuthManager $authManager;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->authManager = new AuthManager();
+    }
+
+    public function testWithoutAuthToken(): void
+    {
+        $this->expectException(MissingAuthException::class);
+        $this->authManager->getPassword('challenge');
+    }
+
+    public function testWithAuthTokenNoChallenge(): void
+    {
+        $this->assertInstanceOf(
+            AuthManager::class,
+            $this->authManager->setAuthToken('token'),
+        );
+        $this->expectException(MissingAuthException::class);
+        $this->authManager->getPassword();
+    }
+
+    public function testWithAuthTokenAndChallengeArg(): void
+    {
+        $this->assertInstanceOf(
+            AuthManager::class,
+            $this->authManager->setAuthToken('token'),
+        );
+
+        $this->assertEquals(self::HASH, $this->authManager->getPassword('challenge'));
+    }
+
+    public function testSetHasChallenge(): void
+    {
+        $this->assertFalse($this->authManager->hasChallenge());
+
+        $this->assertInstanceOf(
+            AuthManager::class,
+            $this->authManager->setChallenge('challenge'),
+        );
+
+        $this->assertTrue($this->authManager->hasChallenge());
+    }
+
+    public function testWithAuthTokenAndChallengeProp(): void
+    {
+        $this->authManager
+            ->setAuthToken('token')
+            ->setChallenge('challenge');
+        $result = $this->authManager->getPassword();
+
+        $this->assertEquals(self::HASH, $result);
+    }
+
+    public function testSetGetSessionToken(): void
+    {
+        $this->assertNull($this->authManager->getSessionToken());
+
+        $this->assertInstanceOf(
+            AuthManager::class,
+            $this->authManager->setSessionToken('sesstok'),
+        );
+
+        $this->assertEquals('sesstok', $this->authManager->getSessionToken());
+    }
+
+    public function testSetHasPermission(): void
+    {
+        $this->assertFalse($this->authManager->hasPermission('foo'));
+        $this->assertInstanceOf(
+            AuthManager::class,
+            $this->authManager->setPermissions(['foo', 'bar']),
+        );
+        $this->assertTrue($this->authManager->hasPermission('foo'));
+        $this->assertFalse($this->authManager->hasPermission('baz'));
+    }
+}
